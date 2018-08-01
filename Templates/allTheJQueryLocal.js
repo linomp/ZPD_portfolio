fe332fce75b8f06fdc0696970f108dd1c804661d
Lino_MP <lm2p@hotmail.es>
2018-08-01 13:16:41 -0500

    first commit

---
+++ Templates/allTheJQueryLocal.js
@@ -0,0 +1,21 @@
+$("#events").on("click", function(){
+    $(this).css("color", "purple"); 
+}); 
+
+
+/*
+(function ($, window, document, undefined) {
+
+  $(function () {
+    $("#navbar_div").load("navbar.html");
+    $("#footer_div").load("footer.html");
+  });
+
+})(jQuery, window, document);
+*/
+
+
+$(document).ready(function(){
+    $( "#navbar_div" ).load( "navbar.html" );
+    $("#footer_div").load("footer.html");
+}); 
