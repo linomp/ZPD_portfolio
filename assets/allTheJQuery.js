82d3c4324a06f1308ce1292bc8073a29f6c2a981
Lino_MP <lm2p@hotmail.es>
2018-08-01 13:51:19 -0500

    move stuff to assets folder

--- assets/allTheJQuery.js
+++ assets/allTheJQuery.js
@@ -16,6 +16,6 @@ $("#events").on("click", function(){
 
 
 $(document).ready(function(){
-    $( "#navbar_div" ).load( "../navbar.html");
-    $("#footer_div").load("../footer.html");
+    $( "#navbar_div" ).load( "navbar.html");
+    $("#footer_div").load("footer.html");
 });
